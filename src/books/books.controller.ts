6bdb2138313557b739e41f9fe2ad9cb0d3cc6ad5
idiotricks <idiotricks@yandex.com>
2020-02-05 23:51:32 +0700

    Fisrt CRUD

---
+++ src/books/books.controller.ts
@@ -0,0 +1,31 @@
+import { Controller, Get, Param, Post, Body, Delete, Query } from '@nestjs/common';
+import { BooksService } from './books.service';
+
+@Controller('books')
+export class BooksController {
+  constructor(private booksService: BooksService) {}
+
+  @Get()
+  async getBooks() {
+    const books = await this.booksService.getBooks();
+    return books;
+  }
+
+  @Get(':bookID')
+  async getBook(@Param('bookID') bookID) {
+    const book = await this.booksService.getBook(bookID);
+    return book;
+  }
+
+  @Post()
+  async addBook(@Body() createBookDTO, CreateBookDTO) {
+    const book = await this.booksService.addBook(createBookDTO);
+    return book;
+  }
+
+  @Delete()
+  async deleteBook(@Query() query) {
+    const books = await this.booksService.deleteBook(query.bookID);
+    return books;
+  }
+}
